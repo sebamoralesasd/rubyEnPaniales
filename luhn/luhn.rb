dce6ee7cee89a1ef0c33f85df887e42069459add
sebamoralesasd <sebasuni96@gmail.com>
2021-01-14 23:51:21 -0300

    FIX: warnings de rubocop

--- luhn/luhn.rb
+++ luhn/luhn.rb
@@ -6,27 +6,31 @@ To get started with TDD, see the `README.md` file in your
 `ruby/luhn` directory.
 =end
 
+# Clase Luhn
 class Luhn
-  def self.valid?(string)
-    newString = string.gsub(/\s+/, "")
-
-    if (newString =~ /[^0-9]/) != nil || newString.length <= 1
-      return false
-    end
-
-    cardNumbers = newString.split(//).map {|ch| ch.to_i }
+  def getCardNumbers(string)
+    cardNumbers = string.split(//).map(&:to_i)
 
     # Si la cantidad de números es par, entonces la cifra más
     # significativa tiene posición impar
     isFirstDigitIndexEven = cardNumbers.length % 2
 
-    cardNumbers.each_index { |index| 
-      if (index + isFirstDigitIndexEven) % 2 == 0
+    cardNumbers.each_index do |index|
+      if (index + isFirstDigitIndexEven).even?
         newValue = cardNumbers[index] * 2
         cardNumbers[index] = newValue > 9 ? newValue - 9 : newValue
       end
-    }
+    end
 
-    return cardNumbers.reduce(:+) % 10 == 0
+    cardNumbers
+  end
+
+  def self.valid?(string)
+    newString = string.gsub(/\s+/, '')
+    return false unless (newString =~ /[^0-9]/).nil? && newString.length > 1
+
+    cardNumbers = getCardNumbers(newString)
+
+    (cardNumbers.reduce(:+) % 10).zero?
   end
 end
